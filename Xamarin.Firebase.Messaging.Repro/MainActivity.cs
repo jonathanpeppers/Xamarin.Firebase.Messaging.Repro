a875c40d723771e47e31e703c375604d46907ee9
Jonathan Peppers <jonathan.peppers@microsoft.com>
2020-06-10 15:08:43 -0500

    Blank project

---
+++ Xamarin.Firebase.Messaging.Repro/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace Xamarin.Firebase.Messaging.Repro
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            global::Firebase.Messaging.FirebaseMessaging foo;
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
